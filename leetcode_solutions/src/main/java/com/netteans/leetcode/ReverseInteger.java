8f13e1863c888d6063ccc01635e7b2dd6b9f46bd
Tean <netteans@163.com>
2019-11-16 02:14:37 +0800

    init no3

--- leetcode_solutions/src/main/java/com/netteans/leetcode/ReverseInteger.java
+++ leetcode_solutions/src/main/java/com/netteans/leetcode/ReverseInteger.java
@@ -1,4 +1,35 @@
 package com.netteans.leetcode;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ReverseInteger {
+
+
+    public int reverse(int x) {
+        int[] ars = new int[0];
+        while (true) {
+            int n = x % 10;
+            int[] nars = new int[ars.length + 1];
+            for (int i = 0; i < ars.length; i++) {
+                nars[i] = ars[i];
+            }
+            nars[ars.length] = n;
+            ars = nars;
+            x /= 10;
+            if (x == 0)
+                break;
+        }
+
+        long r = 0;
+        for (int i = 0; i < ars.length; i++) {
+            long c = ars[i];
+            for (int j = 0; j < ars.length - i - 1; j++)
+                c *= 10;
+            if (c > Integer.MAX_VALUE || c < Integer.MIN_VALUE) return 0;
+            r += c;
+            if (r > Integer.MAX_VALUE || r < Integer.MIN_VALUE) return 0;
+        }
+        return (int) r;
+    }
 }
--- leetcode_solutions/src/test/java/com/netteans/leetcode/test/SolutionTest.java
+++ leetcode_solutions/src/test/java/com/netteans/leetcode/test/SolutionTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+package com.netteans.leetcode.test;
+
+
+import com.netteans.leetcode.ReverseInteger;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SolutionTest {
+    @Test
+    public void test() {
+        int[] arrs = new int[]{1, 2, 3};
+        List l = Arrays.asList(arrs);
+        l.add(4);
+        System.out.println(new ReverseInteger().reverse(1563847412));
+    }
 }
